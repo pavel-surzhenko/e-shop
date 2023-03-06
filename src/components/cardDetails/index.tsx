72e2a9e9375df9d9566cd2ee0a8c7a80dd698746
Pavlo Surzhenko <pavel.surzhenko@icloud.com>
2023-03-06 21:42:29 +0200

    add skeleton to cardDetails

--- src/components/cardDetails/index.tsx
+++ src/components/cardDetails/index.tsx
@@ -8,6 +8,7 @@ import {
     Rating,
     CardActions,
     Button,
+    Skeleton,
 } from '@mui/material'
 import { useParams } from 'react-router-dom'
 import { useItemDetails } from '../../hooks'
@@ -26,17 +27,28 @@ export const CardDetails = () => {
                     flexDirection: { xs: 'column', sm: 'column', md: 'row' },
                 }}
             >
-                <CardMedia
-                    component='img'
-                    image={data?.image}
-                    alt={data?.title}
-                    sx={{
-                        maxWidth: '300px',
-                        height: '400px',
-                        objectFit: 'contain',
-                        margin: '0 auto',
-                    }}
-                />
+                {isFetched ? (
+                    <CardMedia
+                        component='img'
+                        image={data?.image}
+                        alt={data?.title}
+                        sx={{
+                            maxWidth: '300px',
+                            height: '400px',
+                            objectFit: 'contain',
+                            margin: '20px auto',
+                        }}
+                    />
+                ) : (
+                    <Skeleton
+                        animation='wave'
+                        variant='rectangular'
+                        width={300}
+                        height={400}
+                        sx={{m:'20px auto'}}
+                    />
+                )}
+
                 <Box
                     sx={{
                         display: 'flex',
@@ -45,38 +57,88 @@ export const CardDetails = () => {
                     }}
                 >
                     <CardContent sx={{ flex: '1 0 auto' }}>
-                        <Typography
-                            textAlign='center'
-                            gutterBottom
-                            component='div'
-                            variant='h5'
-                        >
-                            {data?.title}
-                        </Typography>
-                        <Typography
-                            textAlign='center'
-                            gutterBottom
-                            variant='subtitle1'
-                            color='text.secondary'
-                            component='div'
-                        >
-                            {data?.description}
-                        </Typography>
-                        <Rating
-                            name='read-only'
-                            value={data?.rating?.rate ?? 1}
-                            readOnly
-                        />
-                        <CardActions sx={{ justifyContent: 'space-between' }}>
-                            <Typography variant='h6'>{data?.price}$</Typography>
-                            <Button
-                                size='medium'
-                                color='primary'
-                                variant='contained'
-                                endIcon={<ShoppingCart />}
+                        {isFetched ? (
+                            <Typography
+                                textAlign='center'
+                                gutterBottom
+                                component='div'
+                                variant='h5'
                             >
-                                Buy
-                            </Button>
+                                {data?.title}
+                            </Typography>
+                        ) : (
+                            <Skeleton
+                                animation='wave'
+                                variant='rectangular'
+                                width={500}
+                                height={50}
+                                sx={{ mb: '15px' }}
+                            />
+                        )}
+                        {isFetched ? (
+                            <Typography
+                                textAlign='center'
+                                gutterBottom
+                                variant='subtitle1'
+                                color='text.secondary'
+                                component='div'
+                            >
+                                {data?.description}
+                            </Typography>
+                        ) : (
+                            <Skeleton
+                                animation='wave'
+                                variant='rectangular'
+                                width={500}
+                                height={200}
+                                sx={{ mb: '15px' }}
+                            />
+                        )}
+                        {isFetched ? (
+                            <Rating
+                                name='read-only'
+                                value={data?.rating?.rate ?? 1}
+                                readOnly
+                            />
+                        ) : (
+                            <Skeleton
+                                animation='wave'
+                                variant='rectangular'
+                                width={200}
+                                height={50}
+                            />
+                        )}
+                        <CardActions sx={{ justifyContent: 'space-between' }}>
+                            {isFetched ? (
+                                <Typography variant='h6'>
+                                    {data?.price}$
+                                </Typography>
+                            ) : (
+                                <Skeleton
+                                    animation='wave'
+                                    variant='rectangular'
+                                    width={150}
+                                    height={50}
+                                    sx={{ml: '-8px'}}
+                                />
+                            )}
+                            {isFetched ? (
+                                <Button
+                                    size='medium'
+                                    color='primary'
+                                    variant='contained'
+                                    endIcon={<ShoppingCart />}
+                                >
+                                    Buy
+                                </Button>
+                            ) : (
+                                <Skeleton
+                                    animation='wave'
+                                    variant='rectangular'
+                                    width={150}
+                                    height={50}
+                                />
+                            )}
                         </CardActions>
                     </CardContent>
                 </Box>
